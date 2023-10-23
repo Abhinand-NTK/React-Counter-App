358a235ddb9293f45729124f8970b2e3ba846d9f
AbhinandNtk <abhinadntkm@gamail.com>
2023-10-23 13:14:45 +0530

    Improved the Ui using the Bootstrap

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -19,17 +19,23 @@ const Counter = () => {
     }
 
   return (
-    <div>
+        <div className='card' style={{
+            backgroundColor: '#035d2caa',
+            borderRadius: '10px',
+            padding: '20px',
+            boxShadow: '0 0 10px rgba(16, 25, 194, 0.667)',
+            textAlign: 'center',
+          }}>
+ 
+            <h1 className='text-warning'>Counter App</h1>
 
-        <h1>Counter App </h1>
-
-        <div>
-            <button onClick={decrementCount}>-</button>
-            <input type="text" value={count}  name="" id="" />
-            <button onClick={incrementCount}>+</button>
+            <div>
+                <button onClick={decrementCount}>-</button>
+                <h1>{count}</h1>
+                <button onClick={incrementCount}>+</button>
+            </div>
+            
         </div>
-        
-    </div>
   )
 }
 
