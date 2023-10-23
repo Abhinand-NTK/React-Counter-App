f2ab1b5e9420180af178e3bdca6f909ed8be026e
AbhinandNtk <abhinadntkm@gamail.com>
2023-10-23 12:51:16 +0530

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+
+    const [count,Setcount] = useState(0);
+
+    const incrementCount =  () =>{
+        Setcount(count + 1);
+    }
+
+    
+    const decrementCount =  () =>{
+        if(count>0){
+            Setcount(count - 1);
+        }
+        else{
+            alert('The counter is out of range  ')
+        }
+    }
+
+  return (
+    <div>
+
+        <h1>Counter App </h1>
+
+        <div>
+            <button onClick={decrementCount}>-</button>
+            <input type="text" value={count}  name="" id="" />
+            <button onClick={incrementCount}>+</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Counter
